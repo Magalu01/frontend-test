bcb528e8d7e27a693f7aa012a17997587fdd866c
Kelvin <kelvinmusselli22@hotmail.com>
2020-11-05 09:11:49 -0300

    firstapi ok

--- challenge/src/store/actions/Home.js
+++ challenge/src/store/actions/Home.js
@@ -1,13 +1,17 @@
 import API from 'axios';
 import { TYPES } from '../types/Home';
+import url from '../../services/api';
 
 const { HOME } = TYPES;
 
 export const getAllHeroes = () => {
   return (dispatch) => {
-    API.get('/')
+    API.get(
+      `${url}/characters?ts=${process.env.REACT_APP_TIMESTAMP}&apikey=${
+        process.env.REACT_APP_PUB_KEY
+      }&hash=${process.env.REACT_APP_HASH}&limit=${20}`
+    )
       .then((response) => {
-        console.log('teste redux connected');
         dispatch({
           type: HOME,
           payload: response.data,
